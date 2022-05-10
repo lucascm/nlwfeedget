9ef77c289ac0802b3834998b353c5b079b6279ab
lucas <lucas@expansiva.com.br>
2022-05-10 14:43:55 -0300

    add prisma. before query

--- server/src/repositories/prisma/prisma-feedbacks-repository.ts
+++ server/src/repositories/prisma/prisma-feedbacks-repository.ts
@@ -5,7 +5,7 @@ export class PrismaFeedbacksRepository implements FeedbacksRepository {
 
     async create(data: FeedbackCreateData) {
         const {type, comment, screenshot} = data
-
+        await prisma.$connect()
         await prisma.feedback.create({
             data: {
                 type,
